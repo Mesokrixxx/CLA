d9b162b09de6232f1d1bf207a0b6785a1d92930d
fldnadam <fldnadam@gmail.com>
2025-10-20 17:54:39 +0200

    v0.11

--- CLA.h
+++ CLA.h
@@ -1,6 +1,9 @@
 #ifndef CLA_H
 # define CLA_H
 
+# include <stdbool.h>
+# include <stdint.h>
+
 typedef union u_vec2i {
 	struct {
 		int	x;
@@ -10,14 +13,27 @@ typedef union u_vec2i {
 }	t_vec2i;
 
 static inline t_vec2i	vec2i(int x, int y) { return (t_vec2i){{ x, y }}; }
+
 static inline t_vec2i	vec2i_adds(t_vec2i v, int x) { return (t_vec2i){{ v.x + x, v.y + x }}; }
 static inline t_vec2i	vec2i_subs(t_vec2i v, int x) { return (t_vec2i){{ v.x - x, v.y - x }}; }
 static inline t_vec2i	vec2i_muls(t_vec2i v, int x) { return (t_vec2i){{ v.x * x, v.y * x }}; }
 static inline t_vec2i	vec2i_divs(t_vec2i v, int x) { return (t_vec2i){{ v.x / x, v.y / x }}; }
+
 static inline t_vec2i	vec2i_add(t_vec2i v1, t_vec2i v2) { return (t_vec2i){{ v1.x + v2.x, v1.y + v2.y }}; }
 static inline t_vec2i	vec2i_sub(t_vec2i v1, t_vec2i v2) { return (t_vec2i){{ v1.x - v2.x, v1.y - v2.y }}; }
 static inline t_vec2i	vec2i_mul(t_vec2i v1, t_vec2i v2) { return (t_vec2i){{ v1.x * v2.x, v1.y * v2.y }}; }
 static inline t_vec2i	vec2i_div(t_vec2i v1, t_vec2i v2) { return (t_vec2i){{ v1.x / v2.x, v1.y / v2.y }}; }
+
+static inline bool	vec2i_eq(t_vec2i v1, t_vec2i v2) { return (v1.x == v2.x && v1.y == v2.y); }
+
+static inline bool	vec2is_contains(const t_vec2i *vs, uint64_t vcount, t_vec2i v) {
+	for (uint64_t i = 0; i < vcount; i++) {
+		if (vec2i_eq(vs[i], v)) {
+			return true;
+		}
+	}
+	return false;
+}
 
 #define			vec2i_scale vec2i_muls
 
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A C Linear Algebra header-only library
 
-### v0.1
-> Vec2i simple implementation
-
+### v0.11
+> Added:
+> vec2i_eq
+> vec2is_contains
