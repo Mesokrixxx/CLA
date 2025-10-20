2383cc6574eef80cf6b61a2cb319dba600c5f61a
fldnadam <fldnadam@gmail.com>
2025-10-20 19:28:43 +0200

    v0.2

--- CLA.h
+++ CLA.h
@@ -12,6 +12,24 @@ typedef union u_vec2i {
 	int	raw[2];
 }	t_vec2i;
 
+typedef union u_vec2 {
+	struct {
+		float	x;
+		float	y;
+	};
+	float	raw[2];
+}	t_vec2;
+
+typedef union u_mat4 {
+	struct {
+		float	m00, m01, m02, m03;
+    		float	m10, m11, m12, m13;
+    		float	m20, m21, m22, m23;
+    		float	m30, m31, m32, m33;
+	};
+	float raw[16];
+}	t_mat4;
+
 static inline t_vec2i	vec2i(int x, int y) { return (t_vec2i){{ x, y }}; }
 
 static inline t_vec2i	vec2i_adds(t_vec2i v, int x) { return (t_vec2i){{ v.x + x, v.y + x }}; }
@@ -35,6 +53,31 @@ static inline bool	vec2is_contains(const t_vec2i *vs, uint64_t vcount, t_vec2i v
 	return false;
 }
 
-#define			vec2i_scale vec2i_muls
+static inline t_vec2	vec2(float x, float y) { return (t_vec2){{ x, y }}; }
+
+static inline t_mat4	mat4_identity(void) {
+	t_mat4	m = {0};
+
+	m.m00 = 1;
+	m.m11 = 1;
+	m.m22 = 1;
+	m.m33 = 1;
+	return m;
+}
+static inline t_mat4	mat4_ortho(t_vec2 width, t_vec2 height, t_vec2 z) {
+	t_mat4	m = {0};
+	float	rl = width.y - width.x;
+	float	tb = height.y - height.x;
+	float	fn = z.y - z.x;
+
+	m.m00 = 2 / rl;
+	m.m11 = 2 / tb;
+	m.m22 = -(2 / fn);
+	m.m30 = -((width.x + width.y) / rl);
+	m.m31 = -((height.x + height.y) / tb);
+	m.m32 = -((z.x + z.y) / fn);
+	m.m33 = 1;
+	return m;
+}
 
 #endif // CLA_H
--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 A C Linear Algebra header-only library
 
-### v0.11
-> Added:
-> vec2i_eq
-> vec2is_contains
+### v0.2
+Added:
+> types: t_vec2 t_mat4
+> functions: vec2 mat4_identity mat4_ortho
+Removed:
+> defines: vec2i_scale
